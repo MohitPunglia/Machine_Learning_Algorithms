ea7eaf8ca4154cd294f8fdaf135ffab65ca8c75e
Mohit <mohit.punglia@gmail.com>
2024-12-17 23:15:22 +0000

    added KNN partial code

--- KNN/partial_code_KNN.py
+++ KNN/partial_code_KNN.py
@@ -29,3 +29,57 @@ X_train.shape
 
 # %%
 X_test.shape
+
+# %% [markdown]
+# As we are using KNN 2 input colums have very large difference we need to scale it dowm to age's value
+#
+# we will use class standardscaler inside sklearn.preprocessor
+
+# %%
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+
+
+# %%
+X_train = scaler.fit_transform(X_train)
+
+# %%
+X_train
+
+# %%
+X_test = scaler.transform(X_test)
+
+# %%
+X_test
+
+# %% [markdown]
+# Applying KNN algo :
+#
+#  We need to find K by applying square root
+
+# %%
+np.sqrt(X_train.shape[0])
+
+# %%
+k = 17
+
+# %%
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier(n_neighbors=k)
+
+# %%
+# for training our model
+
+knn.fit(X_train, y_train)
+
+# %%
+y_pred = knn.predict(X_test)
+
+# %%
+y_pred.shape
+
+# %%
+# need to check y_test shape to ensure both are same
+y_test.shape
