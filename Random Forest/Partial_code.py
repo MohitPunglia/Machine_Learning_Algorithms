a95c0e4b5e8b787e1c8080c2ddad95292b5624d2
Mohit <mohit.punglia@gmail.com>
2025-01-10 20:45:47 +0000

    Adding Partail Code

--- Random Forest/Partial_code.py
+++ Random Forest/Partial_code.py
@@ -47,3 +47,55 @@ from sklearn.ensemble import RandomForestClassifier
 # %%
 rf = RandomForestClassifier(n_estimators=500, random_state=42)
 rf.fit(X_train, y_train)
+
+plt.figure(figsize=(12, 8))
+x_range = np.linspace(X.min(), X.max(), 100)
+xx1, xx2 = np.meshgrid(x_range, x_range)
+y_hat = rf.predict(np.c_[xx1.ravel(), xx2.ravel()])
+y_hat = y_hat.reshape(xx1.shape)
+plt.contourf(xx1, xx2, y_hat, alpha=0.2)
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap="viridis", alpha=0.7)
+plt.title("Random forest")
+plt.show()
+
+# %% [markdown]
+# working on Regression dataset and testing with Decision tree and Random Forest
+#
+#
+
+# %% [markdown]
+# Generation test data
+
+# %%
+n_train = 150
+n_test = 1000
+noise = 0.1
+
+
+# Generate data
+def f(x):
+    x = x.ravel()
+    return np.exp(-(x**2)) + 1.5 * np.exp(-((x - 2) ** 2))
+
+
+def generate(n_samples, noise):
+    X = np.random.rand(n_samples) * 10 - 5
+    X = np.sort(X).ravel()
+    y = (
+        np.exp(-(X**2))
+        + 1.5 * np.exp(-((X - 2) ** 2))
+        + np.random.normal(0.0, noise, n_samples)
+    )
+    X = X.reshape((n_samples, 1))
+
+    return X, y
+
+
+X_train, y_train = generate(n_samples=n_train, noise=noise)
+X_test, y_test = generate(n_samples=n_test, noise=noise)
+
+# %%
+plt.figure(figsize=(18, 8))
+plt.plot(X_test, f(X_test), "r")
+plt.scatter(X_train, y_train, c="b", s=20)
+plt.xlim([-5, 5])
