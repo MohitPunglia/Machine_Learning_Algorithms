113d219660802a230dc3db099b243ef1848a426e
Mohit <mohit.punglia@gmail.com>
2025-01-28 21:20:45 +0000

    added partial code

--- Gradient Boosting/partial_code.py
+++ Gradient Boosting/partial_code.py
@@ -22,3 +22,31 @@ df
 # %%
 plt.scatter(df["X"], df["y"])
 plt.title("Scatter plot of X and y")
+
+# %% [markdown]
+# Adding new column as mean
+
+# %%
+df["pred1"] = df["y"].mean()
+
+# %%
+df
+
+# %% [markdown]
+# Calculating Resilence
+
+# %%
+df["res1"] = df["y"] - df["pred1"]
+
+# %%
+df
+
+# %% [markdown]
+# Plotting the graph
+
+# %%
+plt.scatter(df["X"], df["y"])
+plt.plot(df["X"], df["pred1"], color="red")
+
+# %% [markdown]
+# Creating object of Decision tree and max nodes as 8(generally we keep it from 8 to 32)
