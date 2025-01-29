2acdce75404e02c9f21edfbef51f7a4cc575f8e6
Mohit <mohit.punglia@gmail.com>
2025-01-29 20:38:28 +0000

    added partial code

--- Gradient Boosting/partial_code.py
+++ Gradient Boosting/partial_code.py
@@ -50,3 +50,64 @@ plt.plot(df["X"], df["pred1"], color="red")
 
 # %% [markdown]
 # Creating object of Decision tree and max nodes as 8(generally we keep it from 8 to 32)
+# %%
+tree1 = DecisionTreeRegressor(max_leaf_nodes=8)
+
+# %%
+tree1.fit(df["X"].values.reshape(100, 1), df["res1"].values)
+
+# %%
+from sklearn.tree import plot_tree
+
+plot_tree(tree1)
+plt.show()
+
+# %%
+# generating X_test
+X_test = np.linspace(-0.5, 0.5, 500).reshape(500)
+
+# %% [markdown]
+# Calculating y_pred by first model output + 2nd model output
+
+# %%
+y_pred = 0.265458 + tree1.predict(X_test.reshape(500, 1))
+
+# %%
+plt.figure(figsize=(15, 5))
+plt.subplot(121)
+plt.plot(X_test, y_pred, color="red")
+plt.scatter(df["X"], df["y"])
+
+# %%
+df["pred2"] = 0.265458 + tree1.predict(df["X"].values.reshape(100, 1))
+
+# %%
+df
+
+# %%
+df["res2"] = df["y"] - df["pred2"]
+
+# %%
+df
+
+# %%
+tree2 = DecisionTreeRegressor(max_leaf_nodes=8)
+
+# %%
+tree2.fit(df["X"].values.reshape(100, 1), df["res2"].values)
+
+# %%
+y_pred = (
+    0.265458
+    + tree1.predict(X_test.reshape(500, 1))
+    + tree2.predict(X_test.reshape(500, 1))
+)
+
+# %%
+plt.figure(figsize=(15, 5))
+plt.subplot(121)
+plt.plot(X_test, y_pred, color="red")
+plt.scatter(df["X"], df["y"])
+plt.title("Prediction of y using 2 trees")
+
+# %%
