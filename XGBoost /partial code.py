776afa44f6fd6afabefcb1db50ba52119061e597
Mohit <mohit.punglia@gmail.com>
2025-02-06 23:42:33 +0530

    added partial code

--- XGBoost /partial code.py
+++ XGBoost /partial code.py
@@ -35,3 +35,70 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 # %%
 X_train.head()
+
+X_train.shape
+
+# %%
+X_test.shape
+
+# %% [markdown]
+# Adding column transformer from SKlearn
+#
+
+# %%
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+
+# %%
+trf1 = ColumnTransformer(
+    [
+        ("imputer", SimpleImputer(), [2]),
+        ("imputer2", SimpleImputer(strategy="most_frequent"), [6]),
+    ],
+    remainder="passthrough",
+)
+
+# %%
+trf1.fit_transform(X_train).shape
+
+# %%
+trf1.named_transformers_
+
+# %%
+trf1.named_transformers_["imputer"].statistics_
+
+# %%
+trf1.named_transformers_["imputer2"].statistics_
+
+# %%
+trf2 = ColumnTransformer(
+    [("ohe", OneHotEncoder(sparse_output=False, handle_unknown="ignore"), [1, 6])]
+)
+
+# %%
+from xgboost import XGBClassifier
+
+
+# %%
+clf = XGBClassifier()
+
+# %%
+from sklearn.pipeline import Pipeline
+
+# %%
+pipe = Pipeline([("step1", trf1), ("step22", trf2), ("step3", clf)])
+
+# %%
+pipe.fit(X_train, y_train)
+
+# %%
+y_pred = pipe.predict(X_test)
+
+# %%
+from sklearn.metrics import accuracy_score
+
+# %%
+accuracy_score(y_test, y_pred)
+
+# %%
