8926f6d520f2912058478a16cd07d082faf097d8
Mohit <mohit.punglia@gmail.com>
2025-02-18 21:40:18 +0530

    Added partial code

--- PCA/partial_code.py
+++ PCA/partial_code.py
@@ -68,3 +68,65 @@ eigen_values, eigen_vectors
 
 # %% [markdown]
 # Plotting the graph
+
+# %%
+%matplotlib inline
+
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import proj3d
+from matplotlib.patches import FancyArrowPatch
+
+
+class Arrow3D(FancyArrowPatch):
+    def init(self, xs, ys, zs, *args, **kwargs):
+        FancyArrowPatch.init(self, (0, 0), (0, 0), *args, **kwargs)
+        self._verts3d = xs, ys, zs
+
+    def do_3d_projection(self, renderer):
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, self.axes.M)
+        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
+        FancyArrowPatch.draw(self, renderer)
+        return np.min(zs)
+
+
+fig = plt.figure(figsize=(7, 7))
+ax = fig.add_subplot(111, projection="3d")
+
+ax.plot(
+    df["feature1"],
+    df["feature2"],
+    df["feature3"],
+    "o",
+    markersize=8,
+    color="blue",
+    alpha=0.2,
+)
+ax.plot(
+    [df["feature1"].mean()],
+    [df["feature2"].mean()],
+    [df["feature3"].mean()],
+    "o",
+    markersize=10,
+    color="red",
+    alpha=0.5,
+)
+for v in eigen_vectors.T:
+    a = Arrow3D(
+        [df["feature1"].mean(), v[0]],
+        [df["feature2"].mean(), v[1]],
+        [df["feature3"].mean(), v[2]],
+        mutation_scale=20,
+        lw=3,
+        arrowstyle="-|>",
+        color="r",
+    )
+    ax.add_artist(a)
+ax.set_xlabel("x_values")
+ax.set_ylabel("y_values")
+ax.set_zlabel("z_values")
+
+plt.title("Eigenvectors")
+
+plt.show()
