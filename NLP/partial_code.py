61e4f6d8b36c0c1cca99197bdf20cf6ff3e98913
Mohit <mohit.punglia@gmail.com>
2025-02-27 22:47:36 +0530

    Added Partial Code.py file

--- NLP/partial_code.py
+++ NLP/partial_code.py
@@ -14,3 +14,25 @@ nlp = spacy.load("en_core_web_sm")
 
 # %%
 doc = nlp("Tesla is looking at buying U.S. startup for $6 million")
+
+# %%
+doc.text
+
+# %%
+doc[-1]
+
+# %%
+doc[0].pos_
+
+# %%
+doc[0].dep_
+
+# %%
+doc[0].tag_
+
+
+# %%
+spacy.explain("PROPN")
+
+# %%
+spacy.explain("NNP")
